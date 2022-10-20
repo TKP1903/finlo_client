80b8afd7e373bf38e63d7c8cea94e1c610a68167
sanjaidebuger <nsksanjai@gmail.com>
2022-10-20 06:13:04 +0530

    minor update

--- src/pages/master.jsx
+++ src/pages/master.jsx
@@ -4,14 +4,13 @@ import { useParams } from "react-router-dom";
 //pages
 import HomePage from "./home/homepage";
 import Documents from "./documnets/documents";
-import UserDocumentsPage from "./documnets/userdocuments";
-import DocumentsPage from "./documnets/userFolders";
-<<<<<<< HEAD
+// import UserDocumentsPage from "./documnets/userdocuments";
+// import DocumentsPage from "./documnets/userFolders";
 import AdminPage from "./admin/admin";
 
-=======
+
 import Profile from "./profile/Profile";
->>>>>>> 84e474b5d94bbdd2d401eab91586b822393efac3
+
 const Master = () => {
   const { type } = useParams();
   console.log({ type });
@@ -19,13 +18,11 @@ const Master = () => {
     <div>
       {type === "home" && <HomePage />}
       {/* {type === "documents" && <DocumentsPage />} */}
-<<<<<<< HEAD
       {type === "documents" && <Documents />}
       {type === "admin" && <AdminPage />}
-=======
-      {type === "documents" && <Docuents />}
+      {/* {type === "documents" && <Docuents />} */}
       {type === "profile" && <Profile/>}
->>>>>>> 84e474b5d94bbdd2d401eab91586b822393efac3
+
     </div>
   );
 };
